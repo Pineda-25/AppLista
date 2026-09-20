b98308455a852228c87699c13c4bc7c26f508169
Pineda-25 <juniorpineda2511@gmail.com>
2026-09-19 19:47:36 -0500

    que se note mas ordenado

--- app/src/main/java/com/example/applistas/BuscadorPersonaje.java
+++ app/src/main/java/com/example/applistas/BuscadorPersonaje.java
@@ -12,9 +12,6 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
@@ -46,12 +43,6 @@ public class BuscadorPersonaje extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_buscador_personaje);
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
         edtIdPersonaje            = findViewById(R.id.edtIdPersonaje);
         btnBuscarPersonaje        = findViewById(R.id.btnBuscarPersonaje);
