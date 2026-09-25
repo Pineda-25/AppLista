15605cf7f1b63d2b3df9562bdd028cd838bbf22c
Pineda-25 <juniorpineda2511@gmail.com>
2026-09-25 12:10:07 -0500

    Adaptador mas completo y corregido

--- app/src/main/java/com/example/applistas/Fragments/Buscador_Personajes_fragment.java
+++ app/src/main/java/com/example/applistas/Fragments/Buscador_Personajes_fragment.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,6 +17,16 @@ import com.example.applistas.R;
 
 public class Buscador_Personajes_fragment extends Fragment {
 
+    Button btnsaludar;
+
+    private void loadUI(@NonNull View view){
+        btnsaludar = view.findViewById(R.id.btnsaludar);
+    }
+
+    private void saludar(){
+        Toast.makeText(requireContext(), "Hola esta es la accion", Toast.LENGTH_SHORT).show();
+    }
+
     public Buscador_Personajes_fragment(){}
 
     //3. Implementar 2 metodos ( constructir > ejecutor)
@@ -23,11 +35,14 @@ public class Buscador_Personajes_fragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_buscador_personaje, container, false);
+        return inflater.inflate(R.layout.fragment_buscador_personaje, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        this.loadUI(view); //Fragment
+        btnsaludar.setOnClickListener(v -> { saludar(); });
     }
 }
